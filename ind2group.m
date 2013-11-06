b0fede6c8b3a4a7e14d761353288e7f17c7fcda7
smarek0502 <smarek0502@gmail.com>
2013-11-06 13:47:31 -0500

    Update ind2group.m
    Final version

--- ind2group.m
+++ ind2group.m
@@ -1,35 +1,61 @@
-function [files,a,b,c,d,y] = ind2group(n,w,path,age) 
-% Script's functions include:
-%   1. Importing individual timeseries of each region
+function [files,a,b,c,d,y] = ind2group(n,w,path,age,nodes) 
+
+% Written by: Scott Marek
+% Last updated: 11/6/2013
+
+
+% What this does:
+%   1. Imports individual timeseries of each region
 %      into Matlab. Should be in the format of 
-%      # of ROIs x length of timeseries
+%      # of ROIs x TRs
 %   2. Correlate every ROI with every other ROI
 %      for each subject.
 %   3. Sort matrices by age if needed.
 %   4. Generate average group correlation matrices
 %      using a sliding boxcar of n subjects. 
-%   Output  y: n x n x g matrix, where n = # of ROIs
-%              and g = # of groups. 
 
-% Written by: Scott Marek
-% Last updated: 11/1/2013
+%-----------------------------------------------------------------------------------------
+  
+%   Output:	files = individual subject files 
+%			a = 1xn cell containing individual timeseries x ROI for each subject
+%			b = nx1 cell containing individual correlation matrices
+%			c = cell containing vector of individual correlation matrices and 
+%				age sorted ascendingly 
+%			d = Vector of age-sorted individual correlation matrices
+%			y = n x n x g matrix of group averaged correlation matrices, 
+%				where n = # of ROIs and g = # of groups.
+%              
+%
+%	Input:  n = number of subjects
+%			w = group size (sliding boxcar size)
+%			path = string containing the path to your individual subject files 
+%			age = column containing ages of subject 
+%					*** Be sure this vector lines up with the order of the files you
+%						are importing ***
+%			nodes = number of nodes (regions of interest)
+
+%------------------------------------------------------------------------------------------
 
 %Import individual subject files. 
 files = dir(path);
-    for i = 3:(n+2)(files),
-        a{i-2} = dlmread([path '/' files(i).name,'\t',3,2]);
+    for i = 4:(n+3)(files),
+        a{i-3} = dlmread([path '/' files(i).name],'\t',3,2);
     end
     
     
 % Now all of your individual subject files are in Matlab.
-% Next step is to correlate each subject's timeseries for
+% Next step is to correlate each subject timeseries for
 % each ROI.
 
 for i = 1:n,
     b{i} = corrcoef(a{i});
 end
 
-c = horzcat(b,age);
+b=b';
+
+age=num2cell(age);
+
+c = [b,age];
 
 % Now that all your matrices are imported and have been
 % correlated, you need to sort them.
@@ -39,15 +65,15 @@ c = horzcat(b,age);
 % Ex call:
 % var = sortcell(b,[2]); 2 refers to column of the variable.
 
-c = sortcell(b,[2]);
-d = cell2mat(c,1);
+c = sortcell(c,[2]);
+d = c(:,1);
 
 % Average group correlation matrices using a sliding boxcar.
-% If you're not interested in using a sliding boxcar,
+% If you are not interested in using a sliding boxcar,
 % comment out what is below. 
 
-for r1=1:n
-    for r2=1:n
+for r1=1:nodes
+    for r2=1:nodes
         for i=0:(length(d)-w)
         y(r1,r2,i+1) = mean ( cellfun(@(x)(x(r1,r2)),d((i+1):(w+i)) ));
         end
@@ -58,8 +84,9 @@ end
 % matrices is 'y'. 
 
 % Use variable, y, for generating network metrics.
-% Script for network metrics: Network_Calculations_script.m
+% Function for network metrics: Func_Net_Calcs.m
 
 
 end
+
 
