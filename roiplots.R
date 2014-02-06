7c910e8a22c416dbb89c127ab1244c70f86b4045
Will Foran <willforan@gmail.com>
2014-02-06 15:27:14 -0500

    update roiplots

--- roiplots.R
+++ roiplots.R
@@ -1,9 +1,26 @@
- # install.packages(c('ggplot2','R.matlab'))
- library(R.matlab)
- library(ggplot2)
- roiidx<-213
- roiXsubj<-readMat('mat/PCs_per_ind.mat')
- age <- readMat('age.mat')
- df <- data.frame(roi=roiXsubj$P[roiidx,],age)
- p <- ggplot(df,aes(x=age,y=roi))+geom_smooth()+theme_bw()
- print(p)
+# install.packages(c('ggplot2','R.matlab'))
+library(R.matlab)
+library(ggplot2)
+
+# read in from matlab
+age     <- readMat('mat/age.mat')$age
+sigrois <- readMat('mat/sigPCidx.mat')$sigPCidx
+
+### put matrix and age into one "long" format data frame
+df <- data.frame(roi=t(roiXsubj$P[sigrois,]))
+df$age <- age
+# move all the rois from columns into rows
+df.reshape <- reshape(df,varying=1:nrow(sigrois),direction='long')
+# rename columns so they make sense
+names(df.reshape) <- c('age','roi','value','sid')
+# rename ROI to again be the name/idx from washu
+df.reshape$roi <- sigrois[df.reshape$roi]
+
+
+# plot all sig rois in a grid
+p.all<-ggplot(df.reshape,aes(x=age,y=value))+geom_smooth()+theme_bw()+facet_wrap(~roi)
+print(p.all)
+
+# just plot roi 
+p.248<-ggplot(subset(df.reshape,roi=="248"), aes(x=age,y=value))+geom_smooth() + theme_bw() + ggtitle("roi 248")
+print(p.248)
