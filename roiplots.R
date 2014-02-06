aaf043a256a0b767c9ee31772e27034e33d3ff70
Scott Marek <Scott@fr-wireless-pittnet-150-212-70-184.wireless.pitt.edu>
2014-02-06 14:25:29 -0500

    updated age

--- roiplots.R
+++ roiplots.R
@@ -1,7 +1,9 @@
  # install.packages(c('ggplot2','R.matlab'))
  library(R.matlab)
  library(ggplot2)
+ roiidx<-213
  roiXsubj<-readMat('mat/PCs_per_ind.mat')
- df <- data.frame(roi=roiXsubj$P[roiidx,], age=1:ncol(roiXsubj$P) )
- p <- ggplot(df,aes(x=age,y=roi))+geom_smooth()
+ age <- readMat('age.mat')
+ df <- data.frame(roi=roiXsubj$P[roiidx,],age)
+ p <- ggplot(df,aes(x=age,y=roi))+geom_smooth()+theme_bw()
  print(p)
