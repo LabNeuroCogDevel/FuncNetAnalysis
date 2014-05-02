05016864a3e2f1fdb880f389c2075eb288ca9628
Will Foran <willforan@gmail.com>
2014-05-02 12:59:29 -0400

    update graph to use roinames

--- graphSigNodeCommChange.pl
+++ graphSigNodeCommChange.pl
@@ -2,6 +2,22 @@
 use strict; use warnings;
 # N.B. run R script first to make txt/PCs.txt
 
+# read roi names
+open my $labelsFH, 'txt/labels_bb264_coords';
+my %roinames;
+while(my @line= split(/\t/, <$labelsFH>)){
+ $roinames{$line[0]} = $line[6]; 
+ my %abbrvs = (Left=>'L', Right=>'R', Middle=>'Mid.',Superior=>'Sup',
+               Temporal=>'Temp',Frontal=>'Front',Inferior=>'Inf',Medial=>'Med',
+                ) ;
+ while( my ($name, $abbrv) = each %abbrvs){
+   $roinames{$line[0]} =~ s/$name/$abbrv./;
+ }
+ $roinames{$line[0]} =~ s/\s+//g;
+}
+close $labelsFH;
+
+
 my %graphs; # store 3 graphs: EC LE AL
 
 open my $PCfh, 'txt/PCs.txt';
@@ -24,13 +40,14 @@ while(my @line= split(/\s+/, <$PCfh>)){
    push @{ $graphs{ $line{grpcmp} }->{ $line{OldCommunity} } }, [$line{ROI},$com,abs($change),$posneg]
  }
 }
+close $PCfh;
 
 # print graph
 for my $graphtype (keys %graphs) {
 
    open my $graphout ,">txt/PC_sigGraph_$graphtype.dot";
    print $graphout "digraph G {\n";
-
+   # TODO: add colored nodes outside subgraph: qw/DM SM V CO FP/
    for my $sgname (qw/DM SM V CO FP/) {
      print  $graphout "\tsubgraph cluster_$sgname {\n";
 
@@ -42,7 +59,7 @@ for my $graphtype (keys %graphs) {
      
      # draw each edge between a node in this subgraph and the subgraph it changes with
      for my $edges (@{ $graphs{$graphtype}->{$sgname} }) {
-       print  $graphout "\t\t@$edges[0] -> cluster_@$edges[1] [width=@$edges[2], color=@$edges[3]]\n"
+       print  $graphout "\t\t$roinames{@$edges[0]} -> @$edges[1] [width=@$edges[2], color=@$edges[3]]\n"
      }
      # close the subgraph
      print  $graphout "\t}\n";
